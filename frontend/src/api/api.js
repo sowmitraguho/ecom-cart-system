c4a11be3afe74855d34f9e76373340148c55cef6
Sowmitra Guha <sowmmochy@gmail.com>
2025-10-30 22:22:35 +0600

    api connected

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://mock-cart-system-backend.onrender.com/api", // backend base URL
+    baseURL: "http://localhost:5000/api", // backend base URL
 });
 
 export default api;
